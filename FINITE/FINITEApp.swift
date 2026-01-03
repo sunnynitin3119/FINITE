cd294a35a2575be48469d13761b120b8a7a5073a
Sunny Nitin <sunnynitin873@gmail.com>
2026-01-03 16:52:31 +0000

    Initial Commit

---
+++ FINITE/FINITEApp.swift
@@ -0,0 +1,17 @@
+//
+//  FINITEApp.swift
+//  FINITE
+//
+//  Created by Nitin Yeligeti on 03/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct FINITEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
